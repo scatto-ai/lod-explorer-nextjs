a8bf130d28f837bc938df58317ab08d3096de3fd
Emanuel Vargas <scattoai@gmail.com>
2025-04-18 23:15:59 -0300

    Update lodData.ts

--- src/data/lodData.ts
+++ src/data/lodData.ts
@@ -1,31 +1,31 @@
-import { LODItem } from './types'; 
+import { LODItem } from './types';
 
 export const lodData: LODItem[] = [
   {
     id: 100,
     name: 'LOD 100',
-    imageUrl: '/images/100.png', 
+    imageUrl: '/images/100.png',
     description:
       'Nível conceitual. O elemento do modelo é representado graficamente com um símbolo ou outra representação genérica...',
   },
   {
     id: 200,
     name: 'LOD 200',
-    imageUrl: '/images/200.png', 
+    imageUrl: '/images/200.png',
     description:
       'Geometria aproximada. O elemento do modelo é representado graficamente dentro do modelo com quantidades, tamanho, forma, localização e orientação aproximados...',
   },
   {
     id: 300,
     name: 'LOD 300',
-    imageUrl: '/images/300.png', 
+    imageUrl: '/images/300.png',
     description:
       'Geometria precisa. O elemento do modelo é representado graficamente dentro do modelo com quantidade, tamanho, forma, localização e orientação específicos...',
   },
   {
     id: 350,
     name: 'LOD 350',
-    imageUrl: '/images/350.png',  
+    imageUrl: '/images/350.png',
     description:
       'Inclui detalhes de modelagem e gráficos necessários para explicar como os elementos se conectam a sistemas próximos ou adjacentes...',
   },
